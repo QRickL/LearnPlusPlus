6b4e483f69ef560ea6c6fc279022a685f8ce8d51
QRickL <rickli1738@gmail.com>
2025-10-21 23:20:14 -0700

    single header

---
+++ src/LPP.h
@@ -0,0 +1,10 @@
+#ifndef LPP_INCLUDE_HPP
+#define LPP_INCLUDE_HPP
+
+// Include this rather than including all of the header files below
+
+#include "network.h"
+#include "layer.h"
+#include "mlp.h"
+
+#endif
