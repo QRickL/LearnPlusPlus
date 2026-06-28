034a88e019a8291c24f32d6199bea6c871c00ed4
QRickL <rickli1738@gmail.com>
2026-06-28 02:52:22 -0400

    change from double to number

--- src/functions/activations.cpp
+++ src/functions/activations.cpp
@@ -1,64 +1,77 @@
 #include "activations.h"
 #include <cmath>
 
-LPP::Activation::~Activation() {}
+template <typename number>
+LPP::Activation<number>::~Activation() {}
 
-double LPP::Identity::apply_itself(double x) const
+template <typename number>
+number LPP::Identity<number>::applyActivation(number x) const
 {
     return x;
 }
 
-double LPP::Identity::apply_derivative(double x) const
+template <typename number>
+number LPP::Identity<number>::applyDerivative(number x) const
 {
     return 1.0;
 }
 
-const std::string& LPP::Identity::who() const {
+template <typename number>
+const std::string& LPP::Identity<number>::who() const {
     return STRING_IDENTITY;
 }
 
-double LPP::ReLU::apply_itself(double x) const
+template <typename number>
+number LPP::ReLU<number>::applyActivation(number x) const
 {
-    return (x >= 0) ? x : 0.0;
+    return (x >= 0.0) ? x : 0.0;
 }
 
-double LPP::ReLU::apply_derivative(double x) const 
+template <typename number>
+number LPP::ReLU<number>::applyDerivative(number x) const 
 {
     // Mathematically speaking, it should be undefined at zero
     // For convenience, we assign it 1
-    return (x >= 0) ? 1 : 0.0;
+    return (x >= 0.0) ? 1 : 0.0;
 }
 
-const std::string& LPP::ReLU::who() const {
+template <typename number>
+const std::string& LPP::ReLU<number>::who() const {
     return STRING_RELU;
 }
 
-double LPP::Sigmoid::apply_itself(double x) const
+template <typename number>
+number LPP::Sigmoid<number>::applyActivation(number x) const
 {
-    return 1 / (1 + std::exp(-x));
+    return 1.0 / (1.0 + std::exp(-x));
 }
 
-double LPP::Sigmoid::apply_derivative(double x) const
+template <typename number>
+number LPP::Sigmoid<number>::applyDerivative(number x) const
 {
-    const double s = LPP::Sigmoid::apply_itself(x);
-    return s * (1-s);
+    const number s = LPP::Sigmoid::applyActivation(x);
+    return s * (1.0-s);
 }
 
-const std::string& LPP::Sigmoid::who() const {
+template <typename number>
+const std::string& LPP::Sigmoid<number>::who() const {
     return STRING_SIGMOID;
 }
 
-double LPP::Tanh::apply_itself(double x) const
+template <typename number>
+number LPP::Tanh<number>::applyActivation(number x) const
 {
     return std::tanh(x);
 }
 
-double LPP::Tanh::apply_derivative(double x) const
+template <typename number>
+number LPP::Tanh<number>::applyDerivative(number x) const
 {
-    const double s = std::cosh(x);
-    return 1 / (s * s);
+    const number s = std::cosh(x);
+    return 1.0 / (s * s);
 }
 
-const std::string& LPP::Tanh::who() const {
+template <typename number>
+const std::string& LPP::Tanh<number>::who() const {
     return STRING_TANH;
 }
--- src/functions/activations.h
+++ src/functions/activations.h
@@ -1,70 +1,82 @@
-#ifndef LPP_ACTIVATIONS_H
-#define LPP_ACTIVATIONS_H
+#pragma once
 
 #include <unordered_map>
 #include <memory>
 #include <string>
 
-// Activation functions are used by the layer and network classes to perform inference and training
-// Available activation functions:
-// * Identity
-// * ReLU
-// * Sigmoid
-// * Tanh
+/*
+
+Activation functions are used by the layer and network classes to perform inference and training
+Available activation functions:
+* Identity
+* ReLU
+* Sigmoid
+* Tanh
+
+Overides for sigmoid or tanh are required if using custom number types
+
+*/
+
+// TODO: who() should now also report the data type
 
 namespace LPP {
 
+template <typename number>
 class Activation {
 public:
-    virtual double apply_itself(double x) const = 0;
-    virtual double apply_derivative(double x) const = 0;
+    virtual number applyActivation(number x) const = 0;
+    virtual number applyDerivative(number x) const = 0;
     virtual const std::string& who() const = 0;
 
     // Activation is abstract
     virtual ~Activation() = 0;
 };
 
-class Identity : public Activation {
+template <typename number>
+class Identity : public Activation<number> {
 public:
-    double apply_itself(double x) const override;
-    double apply_derivative(double x) const override;
+    number applyActivation(number x) const override;
+    number applyDerivative(number x) const override;
     const std::string& who() const override;
 
     ~Identity() {}
 };
 
-class ReLU : public Activation {
+template <typename number>
+class ReLU : public Activation<number> {
 public:
-    double apply_itself(double x) const override;
-    double apply_derivative(double x) const override;
+    number applyActivation(number x) const override;
+    number applyDerivative(number x) const override;
     const std::string& who() const override;
 
     ~ReLU() {}
 };
 
-class Sigmoid : public Activation {
+template <typename number>
+class Sigmoid : public Activation<number> {
 public:
-    double apply_itself(double x) const override;
-    double apply_derivative(double x) const override;
+    number applyActivation(number x) const override;
+    number applyDerivative(number x) const override;
     const std::string& who() const override;
     
     ~Sigmoid() {}
 };
 
-class Tanh : public Activation {
+template <typename number>
+class Tanh : public Activation<number> {
 public:
-    double apply_itself(double x) const override;
-    double apply_derivative(double x) const override;
+    number applyActivation(number x) const override;
+    number applyDerivative(number x) const override;
     const std::string& who() const override;
     
     ~Tanh() {}
 };
 
-// Global variables for construction of models by user
-const auto IDENTITY = std::make_shared<Identity>();
-const auto RELU     = std::make_shared<ReLU>();
-const auto SIGMOID  = std::make_shared<Sigmoid>();
-const auto TANH     = std::make_shared<Tanh>();
+// // Global variables for construction of models by user
+// const auto IDENTITY = std::make_shared<Identity>();
+// const auto RELU     = std::make_shared<ReLU>();
+// const auto SIGMOID  = std::make_shared<Sigmoid>();
+// const auto TANH     = std::make_shared<Tanh>();
 
 // Global variables for construction of models by file
 const std::string STRING_IDENTITY  = "IDENTITY";
@@ -72,13 +84,12 @@ const std::string STRING_RELU      = "RELU";
 const std::string STRING_SIGMOID   = "SIGMOID";
 const std::string STRING_TANH      = "TANH";
 
-const std::unordered_map<std::string, std::shared_ptr<Activation>> choose_activation = {
-    {STRING_IDENTITY,   IDENTITY},
-    {STRING_RELU,       RELU},
-    {STRING_SIGMOID,    SIGMOID},
-    {STRING_TANH,       TANH}
+template <typename number>
+const std::unordered_map<std::string, std::shared_ptr<Activation<number>>> chooseActivation = {
+    {STRING_IDENTITY,   std::make_shared< Identity<number> >},
+    {STRING_RELU,       std::make_shared< ReLU<number> >},
+    {STRING_SIGMOID,    std::make_shared< Sigmoid<number> >},
+    {STRING_TANH,       std::make_shared< Tanh<number> >}
 };
 
 } // namespace LPP
-
-#endif
