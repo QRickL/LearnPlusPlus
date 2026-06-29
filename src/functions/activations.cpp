975aa3168294fb1867719aafa933c013b1042c1e
QRickL <rickli1738@gmail.com>
2026-06-29 00:44:52 -0400

    int to float

--- src/functions/activations.cpp
+++ src/functions/activations.cpp
@@ -1,6 +1,8 @@
 #include "activations.h"
 #include <cmath>
 
+// No need to inline. Compiler will do it for you
+
 LPP::Activation::~Activation() {}
 
 float LPP::Identity::apply_activation(float x) const
@@ -10,7 +12,7 @@ float LPP::Identity::apply_activation(float x) const
 
 float LPP::Identity::apply_derivative(float x) const
 {
-    return 1.0;
+    return 1.f;
 }
 
 const std::string& LPP::Identity::who() const {
@@ -19,14 +21,14 @@ const std::string& LPP::Identity::who() const {
 
 float LPP::ReLU::apply_activation(float x) const
 {
-    return (x >= 0) ? x : 0.0;
+    return (x >= 0.f) ? x : 0.f;
 }
 
 float LPP::ReLU::apply_derivative(float x) const 
 {
     // Mathematically speaking, it should be undefined at zero
     // For convenience, we assign it 1
-    return (x >= 0) ? 1 : 0.0;
+    return (x >= 0.f) ? 1.f : 0.f;
 }
 
 const std::string& LPP::ReLU::who() const {
@@ -35,13 +37,13 @@ const std::string& LPP::ReLU::who() const {
 
 float LPP::Sigmoid::apply_activation(float x) const
 {
-    return 1 / (1 + std::exp(-x));
+    return 1.f / (1.f + std::exp(-x));
 }
 
 float LPP::Sigmoid::apply_derivative(float x) const
 {
-    const float s = LPP::Sigmoid::apply_activation(x);
-    return s * (1-s);
+    float s = LPP::Sigmoid::apply_activation(x);
+    return s * (1.f-s);
 }
 
 const std::string& LPP::Sigmoid::who() const {
@@ -55,8 +57,8 @@ float LPP::Tanh::apply_activation(float x) const
 
 float LPP::Tanh::apply_derivative(float x) const
 {
-    const float s = std::cosh(x);
-    return 1 / (s * s);
+    float s = std::cosh(x);
+    return 1.f / (s * s);
 }
 
 const std::string& LPP::Tanh::who() const {
