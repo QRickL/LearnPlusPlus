f815b0a304abcfd25bd56f7b57740980f8496b60
QRickL <rickli1738@gmail.com>
2026-06-29 22:42:49 -0400

    oops

---
+++ src/checking/check.cpp
@@ -0,0 +1,11 @@
+#include "check.h"
+
+void __lpp_check__(bool condition, const std::string& error_message)
+{
+    if (!condition) throw std::runtime_error(error_message);
+}
+
+void LPP::enforce_condition(bool condition, const std::string& error_message)
+{
+    if (!condition) throw std::runtime_error(error_message);
+}
