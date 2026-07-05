3c0fad4ef98fea5925ec652e9a67390f2fb17a28
QRickL <rickli1738@gmail.com>
2026-07-05 11:47:34 -0400

    move impls to cpp

---
+++ src/options/training_options.cpp
@@ -0,0 +1,60 @@
+#include "training_options.hpp"
+#include "../checking/check.hpp"
+
+LPP::ExtraTrainingOptions::ExtraTrainingOptions() :
+    mini_batch_size_option_{nullptr},
+    training_output_stream_{&std::cout},
+    training_metadata_stream_{nullptr},
+    use_validation_option_{false},
+    validation_features_{nullptr},
+    validation_responeses_{nullptr}
+{}
+
+void LPP::ExtraTrainingOptions::set_mini_batch_size(size_t s) {
+    enforce_condition(s != 0,
+            "ExtraTrainingOptions::set_mini_batch_size - batch size cannot be 0");
+
+    mini_batch_size_option_ = std::make_unique<size_t>(s);
+}
+
+void LPP::ExtraTrainingOptions::set_training_output_stream(std::ostream& os) {
+    training_output_stream_ = &os;
+}
+
+void LPP::ExtraTrainingOptions::set_training_metadata_stream(std::ostream& os) {
+    training_metadata_stream_ = &os;
+}
+
+void LPP::ExtraTrainingOptions::set_validation_data(
+    const Matrix& validation_features,
+    const Matrix& validation_responses
+) {
+    enforce_condition(validation_features.rows() == validation_responses.rows(),
+        "ExtraTrainingOptions::set_validation_data - validaation features and reponses have different number of rows");
+
+    validation_features_ = &validation_features;
+    validation_responeses_ = &validation_responses; // TODO: check if this is like... bad for me
+    use_validation_option_ = true;
+}
+
+
+void LPP::ExtraTrainingOptions::set_regularization(std::shared_ptr<regular::Regularizer> r) {
+    regularization_option_ = r;
+}
+
+
+bool LPP::ExtraTrainingOptions::use_mini_batch() const { return mini_batch_size_option_ != nullptr; }
+size_t LPP::ExtraTrainingOptions::mini_batch_size() const { return *mini_batch_size_option_; }
+
+bool LPP::ExtraTrainingOptions::has_output_stream() const { return training_output_stream_ != nullptr; }
+std::ostream& LPP::ExtraTrainingOptions::output_stream() const { return *training_output_stream_; }
+
+bool LPP::ExtraTrainingOptions::has_metadata_stream() const { return training_metadata_stream_ != nullptr; }
+std::ostream& LPP::ExtraTrainingOptions::metadata_stream() const { return *training_metadata_stream_; }
+
+bool LPP::ExtraTrainingOptions::use_validation() const { return use_validation_option_; }
+const LPP::Matrix& LPP::ExtraTrainingOptions::validation_features() const { return *validation_features_; }
+const LPP::Matrix& LPP::ExtraTrainingOptions::validation_responses() const { return *validation_responeses_; }
+
+bool LPP::ExtraTrainingOptions::use_regularization() const { return regularization_option_ != nullptr; }
+const std::shared_ptr<LPP::regular::Regularizer>& LPP::ExtraTrainingOptions::regularizer() const { return regularization_option_; }
--- src/options/training_options.hpp
+++ src/options/training_options.hpp
@@ -2,9 +2,8 @@
 #define LPP_TRAINING_OPTION_HPP
 
 #include <cstddef>
-#include "../regular/regularizer.hpp"
 #include <iostream>
-#include "../checking/check.hpp"
+#include "../regular/regularizer.hpp"
 
 namespace LPP
 {
@@ -31,63 +30,28 @@ class ExtraTrainingOptions {
 
 public:
 
-ExtraTrainingOptions() :
-    mini_batch_size_option_{nullptr},
-    training_output_stream_{&std::cout},
-    training_metadata_stream_{nullptr},
-    use_validation_option_{false},
-    validation_features_{nullptr},
-    validation_responeses_{nullptr}
-{}
+ExtraTrainingOptions();
 
-void set_mini_batch_size(size_t s) {
-    enforce_condition(s != 0,
-            "ExtraTrainingOptions::set_mini_batch_size - batch size cannot be 0");
+void set_mini_batch_size(size_t s);
+bool use_mini_batch() const;
+size_t mini_batch_size() const;
 
-    mini_batch_size_option_ = std::make_unique<size_t>(s);
-}
+void set_training_output_stream(std::ostream& os);
+bool has_output_stream() const;
+std::ostream& output_stream() const;
 
-void set_training_output_stream(std::ostream& os) {
-    training_output_stream_ = &os;
-}
+void set_training_metadata_stream(std::ostream& os);
+bool has_metadata_stream() const;
+std::ostream& metadata_stream() const;
 
-void set_training_metadata_stream(std::ostream& os) {
-    training_metadata_stream_ = &os;
-}
+void set_validation_data(const Matrix& validation_features, const Matrix& validation_responses);
+bool use_validation() const;
+const Matrix& validation_features() const;
+const Matrix& validation_responses() const;
 
-void set_validation_data(
-    const Matrix& validation_features,
-    const Matrix& validation_responses
-) {
-    enforce_condition(validation_features.rows() == validation_responses.rows(),
-        "ExtraTrainingOptions::set_validation_data - validaation features and reponses have different number of rows");
-
-    validation_features_ = &validation_features;
-    validation_responeses_ = &validation_responses; // TODO: check if this is like... bad for me
-    use_validation_option_ = true;
-}
-
-
-void set_regularization(std::shared_ptr<regular::Regularizer> r) {
-    regularization_option_ = r;
-}
-
-
-bool use_mini_batch() const { return mini_batch_size_option_ != nullptr; }
-size_t mini_batch_size() const { return *mini_batch_size_option_; }
-
-bool has_output_stream() const { return training_output_stream_ != nullptr; }
-std::ostream& output_stream() const { return *training_output_stream_; }
-
-bool has_metadata_stream() const { return training_metadata_stream_ != nullptr; }
-std::ostream& metadata_stream() const { return *training_metadata_stream_; }
-
-bool use_validation() const { return use_validation_option_; }
-const Matrix& validation_features() const { return *validation_features_; }
-const Matrix& validation_responses() const { return *validation_responeses_; }
-
-bool use_regularization() const { return regularization_option_ != nullptr; }
-const std::shared_ptr<regular::Regularizer>& regularizer() const { return regularization_option_; }
+void set_regularization(std::shared_ptr<regular::Regularizer> r);
+bool use_regularization() const;
+const std::shared_ptr<regular::Regularizer>& regularizer() const;
 
 
 };
